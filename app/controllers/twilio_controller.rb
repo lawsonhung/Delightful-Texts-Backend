e99c9f6756c62088688c78ca5948efc65dcc5c27
lawsonhung <lawsonhung@yahoo.com>
2020-04-01 18:00:56 -0400

    Error after_filter Unable to load application: NoMethodError: undefined method

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,7 +3,7 @@ require 'twilio-ruby'
 class TwilioController < ApplicationController
   include Webhookable
 
-  after_filter :set_header
+  # after_filter :set_header
 
   skip_before_action :verify_authenticity_token
   # skip_before_action :authorized
