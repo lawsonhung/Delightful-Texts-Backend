fbee7054b787ab93a8d13ee87cf06833ee9066b9
lawsonhung <lawsonhung@yahoo.com>
2020-04-14 17:23:37 -0400

    Changing puma.rb and Procfile back to Ruby Starter code to upload to Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -1,68 +1,74 @@
-# # Puma can serve each request in a thread from an internal thread pool.
-# # The `threads` method setting takes two numbers: a minimum and maximum.
-# # Any libraries that use thread pools should be configured to match
-# # the maximum value specified for Puma. Default is set to 5 threads for minimum
-# # and maximum; this matches the default thread size of Active Record.
-# #
-# max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-# min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-# threads min_threads_count, max_threads_count
+####################################### Original Ruby Starter code start
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
 
 
-# ######### Heroku
+######### Heroku
 # rackup      DefaultRackup
 
-# # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-# #
-# port        ENV.fetch("PORT") { 3000 }
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port        ENV.fetch("PORT") { 3000 }
 
-# # Specifies the `environment` that Puma will run in.
-# #
-# environment ENV.fetch("RAILS_ENV") { "development" }
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-# # Specifies the `pidfile` that Puma will use.
-# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# # Specifies the number of `workers` to boot in clustered mode.
-# # Workers are forked web server processes. If using threads and workers together
-# # the concurrency of the application would be max `threads` * `workers`.
-# # Workers do not work on JRuby or Windows (both of which do not support
-# # processes).
-# #
-# ########### Heroku
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked web server processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+########### Heroku
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-# # Use the `preload_app!` method when specifying a `workers` number.
-# # This directive tells Puma to first boot the application and load code
-# # before forking the application. This takes advantage of Copy On Write
-# # process behavior so workers use less memory.
-# #
-# ############# Heroku
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+############# Heroku
 # preload_app!
 
-# ############# Heroku
+############# Heroku
 # on_worker_boot do
 #   # Worker specific setup for Rails 4.1+
 #   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
 #   ActiveRecord::Base.establish_connection
 # end
 
-# # Allow puma to be restarted by `rails restart` command.
-# plugin :tmp_restart
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
 
-######################################## Heroku: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#adding-puma-to-your-application
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
+####################################### Original Ruby Starter code end
 
-preload_app!
+# ######################################## Heroku Start
+# # Heroku: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#adding-puma-to-your-application
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+# threads threads_count, threads_count
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+# preload_app!
 
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+# rackup      DefaultRackup
+# port        ENV['PORT']     || 3000
+# environment ENV['RACK_ENV'] || 'development'
+
+# on_worker_boot do
+#   # Worker specific setup for Rails 4.1+
+#   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+#   ActiveRecord::Base.establish_connection
+# end
+
+# ######################################## Heroku End
